ea7732a63dbdc20ab39531ddf6b286a651c93c2f
Caleb szajnuk <71284740+cAE-Leb@users.noreply.github.com>
2023-08-15 10:32:09 -0400

    added functionality to determine win conditions, and tie conditions. Added main game loop, game is playable with winners now

--- connect4.py
+++ connect4.py
@@ -3,15 +3,13 @@ This program will be a terminal based version of the popular two player board ga
 A standard 7 x 6 sized board will be used. https://en.wikipedia.org/wiki/Connect_Four
 """
 
-# Initialize the Board
+# global variables
 NUM_ROWS = 6
 NUM_COLS = 7
 EMPTY = ' '
 PLAYER_1 = 'X'
 PLAYER_2 = 'O'
 
-board = [[EMPTY for _ in range(NUM_ROWS)] for _ in range(NUM_COLS)]
-#print(board)
 
 def print_column_numbers():
     for col in range(NUM_COLS):
@@ -32,8 +30,6 @@ def display_board(board):
     print_board(board)
 
 
-current_player = PLAYER_1
-
 def get_player_input(player):
     return int(input(f"Player {player}, choose a column (0 - {NUM_COLS-1}): "))
 
@@ -51,18 +47,82 @@ def place_piece(board, column, player):
             break
 
 
+def consecutive_cells_are_same(cell1, cell2, cell3, cell4):
+    if cell1 == cell2 == cell3 == cell4 and cell1 != EMPTY:
+        return True
+    return False
+
+
+def check_horizontal(board):
+    for row in range(NUM_ROWS):
+        for col in range(NUM_COLS - 3): # -3 to stay within board boundary
+            if consecutive_cells_are_same(board[col][row], board[col + 1][row], board[col + 2][row], board[col + 3][row]):
+                return True
+    return False
+        
+
+def check_vertical(board):
+    for col in range(NUM_COLS):
+        for row in range(NUM_ROWS - 3):  # -3 to stay within board limits
+            if consecutive_cells_are_same(board[col][row], board[col][row + 1], board[col][row + 2], board[col][row + 3]):
+                return True
+    return False
+
+
+def check_diagonal_left_to_right(board):
+    for col in range(NUM_COLS - 3):
+        for row in range(NUM_ROWS - 3):
+            if consecutive_cells_are_same(board[col][row], board[col + 1][row + 1], board[col + 2][row + 2], board[col + 3][row + 3]):
+                return True
+    return False
+
+
+def check_diagonal_right_to_left(board):
+    for col in range(NUM_COLS - 3):
+        for row in range(3, NUM_ROWS):  # Start from the 4th row
+            if consecutive_cells_are_same(board[col][row], board[col + 1][row - 1], board[col + 2][row - 2], board[col + 3][row - 3]):
+                return True
+    return False
+
+
+def check_win(board):
+    if (check_horizontal(board) or
+        check_vertical(board) or
+        check_diagonal_left_to_right(board) or
+        check_diagonal_right_to_left(board)):
+        return True
+    return False
+
+
+
 if __name__ == "__main__":
+    #initialize board
     board = [[EMPTY for _ in range(NUM_ROWS)] for _ in range(NUM_COLS)]
     current_player = PLAYER_1
-    
-    for _ in range(NUM_ROWS * NUM_COLS):  # This will allow for the maximum number of moves in a game
+
+    #flag to check if game is still ongoing
+    game_over = False
+
+
+    #main loop
+    while not game_over:
         display_board(board)
+
         column = get_player_input(current_player)
-        
+
         while not is_valid_move(board, column):
             print("Invalid move. Try again.")
             column = get_player_input(current_player)
-        
-        place_piece(board, column, current_player)
-        current_player = PLAYER_2 if current_player == PLAYER_1 else PLAYER_1  # Switch player for next turn
 
+        place_piece(board, column, current_player)
+        if check_win(board):
+            display_board(board) # show the final winning board
+            print(f"Player {current_player} wins!")
+            game_over = True
+        else:
+            current_player = PLAYER_2 if current_player == PLAYER_1 else PLAYER_1
+
+        if all([cell != EMPTY for row in board for cell in row]):
+            display_board(board)
+            print("Its a tie!")
+            game_over = True
